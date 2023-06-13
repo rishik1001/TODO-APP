741df7afbd4399dd38acff584c0b1874491e57e7
rishik1001 <122955396+rishik1001@users.noreply.github.com>
2023-06-13 09:48:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [process.env.FRONTEND_URI],
     methods: ["GET","POST","PUT","DELETE"],
-    credentials: true //if false then response headers will not be sent
+    withCredentials: true //if false then response headers will not be sent
 }))
 
 app.use("/api/v1/users",userRouter); //for saying we are using api
